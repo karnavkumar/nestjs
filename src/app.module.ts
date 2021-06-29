c38199edc6cb23c61c8aa3de063d1d3404f331fe
Karnav Pargi <karnav.pargi@gmail.com>
2021-06-29 22:50:23 +0530

    Added validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './exception.filter';
-import { Module } from '@nestjs/common';
+
+import { Module, ValidationPipe } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './components/user/user.module';
 import { AuthModule } from './auth/auth.module';
-
+import { APP_PIPE } from '@nestjs/core';
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, AuthModule],
   controllers: [],
   providers: [
     {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
     },
   ],
 })
