ebe66f3aa484c8563832594061e9d8f64f6390ac
Karnav Pargi <karnav.pargi@gmail.com>
2021-06-29 16:03:38 +0530

    Added initial auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './components/user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, AuthModule],
   controllers: [],
   providers: [
     {
