97e259901e6a26d08192081a4cf2b268c69a579f
Karnav Pargi <karnav.pargi@gmail.com>
2021-06-30 21:00:41 +0530

    updated app module for Authentication and authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
-import { APP_FILTER } from '@nestjs/core';
-
 import { Module, ValidationPipe } from '@nestjs/common';
-
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { UserModule } from './components/user/user.module';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { APP_PIPE } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UserLoginModule } from './components/user-login/user-login.module';
+import { UserModule } from './components/user/user.module';
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, AuthModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    UserLoginModule,
+  ],
+  controllers: [AppController],
   providers: [
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
+    { provide: APP_PIPE, useClass: ValidationPipe },
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
   ],
 })
 export class AppModule {}
